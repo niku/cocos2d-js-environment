ceb89c7ac1e28203fdb07720334c9d34b5a35a16
niku <niku@niku.name>
2015-02-11 05:30:26 +0900

    Extract version as a variable

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 user=vagrant
 home=/home/${user}
-cocos2d=cocos2d-js-v3.2
+version=`cat /vagrant/version`
 
 # install dependent packages
 if ! type unzip > /dev/null 2>&1; then
@@ -10,16 +10,16 @@ if ! type unzip > /dev/null 2>&1; then
 fi
 
 # download and unzip cocos2d-js
-if [ ! -e ${home}/${cocos2d} ]; then
+if [ ! -e ${home}/cocos2d-js-${version} ]; then
     cd /var/tmp
-    sudo -u vagrant curl -O http://cdn.cocos2d-x.org/${cocos2d}.zip
+    sudo -u vagrant curl -O http://cdn.cocos2d-x.org/cocos2d-js-${version}.zip
     cd ${home}
-    sudo -u vagrant unzip /var/tmp/${cocos2d}.zip
+    sudo -u vagrant unzip /var/tmp/cocos2d-js-${version}.zip
 fi
 
 # setup cocos2d-js
 if ! grep COCOS_CONSOLE_ROOT ${home}/.bashrc > /dev/null 2>&1; then
-    cd ${home}/${cocos2d}
+    cd ${home}/cocos2d-js-${version}
     yes '' | sudo -u vagrant ./setup.py
 fi
 
